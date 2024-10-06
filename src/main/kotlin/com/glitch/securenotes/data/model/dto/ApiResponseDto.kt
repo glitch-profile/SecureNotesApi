8259c0e68e7ecf630cc4f9cf0506ca15908d28c8
Ivan <korablev2002@yandex.ru>
2024-10-06 14:05:42 +0400

    updated ApiResponseDto serialization

--- src/main/kotlin/com/glitch/securenotes/data/model/dto/ApiResponseDto.kt
+++ src/main/kotlin/com/glitch/securenotes/data/model/dto/ApiResponseDto.kt
@@ -3,30 +3,27 @@ package com.glitch.securenotes.data.model.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed class ApiResponseDto<T>(
-    val data: T?,
-    val status: Boolean,
-    val apiErrorCode: Int? = null,
-    val message: String
-) {
-    class Success<T>(
-        data: T?,
-        message: String = "Ok",
-        apiErrorCode: Int? = null,
-    ): ApiResponseDto<T>(
-        data = data,
-        status = true,
-        apiErrorCode = apiErrorCode,
-        message = message
-    )
-    class Error<T>(
-        apiErrorCode: Int,
-        message: String,
-        data: T? = null
-    ): ApiResponseDto<T>(
-        data = data,
-        status = false,
-        apiErrorCode = apiErrorCode,
-        message = message
-    )
+sealed class ApiResponseDto<T> {
+    abstract val data: T?
+    abstract val status: Boolean
+    abstract val apiErrorCode: Int?
+    abstract val message: String
+
+    @Serializable
+    data class Success<T>(
+        override val data: T?,
+        override val message: String = "Ok",
+        override val apiErrorCode: Int? = null,
+    ): ApiResponseDto<T>() {
+        override val status = true
+    }
+
+    @Serializable
+    data class Error<T>(
+        override val apiErrorCode: Int,
+        override val message: String,
+        override val data: T? = null
+    ): ApiResponseDto<T>() {
+        override val status = false
+    }
 }
