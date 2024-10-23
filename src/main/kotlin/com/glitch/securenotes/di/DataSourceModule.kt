23d970c1eb1e5744249e7ff353b79faaeeb20194
Ivan <korablev2002@yandex.ru>
2024-10-24 00:26:38 +0500

    refactoring

--- src/main/kotlin/com/glitch/securenotes/di/DataSourceModule.kt
+++ src/main/kotlin/com/glitch/securenotes/di/DataSourceModule.kt
@@ -4,12 +4,10 @@ import com.glitch.securenotes.data.datasource.UserCredentialsDataSource
 import com.glitch.securenotes.data.datasource.UsersDataSource
 import com.glitch.securenotes.data.datasource.notes.NoteResourcesDataSource
 import com.glitch.securenotes.data.datasource.notes.NotesDataSource
-import com.glitch.securenotes.data.datasourceimpl.notes.NotesDataSourceImpl
 import com.glitch.securenotes.data.datasourceimpl.UserCredentialsDataSourceImpl
 import com.glitch.securenotes.data.datasourceimpl.UsersDataSourceImpl
+import com.glitch.securenotes.data.datasourceimpl.notes.NotesDataSourceImpl
 import com.glitch.securenotes.data.datasourceimpl.notes.NotesResourcesDataSourceImpl
-import com.glitch.securenotes.domain.utils.notescache.NoteInfoCacheManager
-import com.glitch.securenotes.domain.utils.notescache.NoteInfoCacheManagerImpl
 import org.koin.dsl.module
 
 val dataSourceModule = module {
