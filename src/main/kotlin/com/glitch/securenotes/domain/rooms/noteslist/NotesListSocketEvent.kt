0f743a25f1392bd1b46a14de21a10092e17a6ca5
Ivan <korablev2002@yandex.ru>
2024-11-17 15:13:11 +0400

    updated data model

--- src/main/kotlin/com/glitch/securenotes/domain/rooms/noteslist/NotesListSocketEvent.kt
+++ src/main/kotlin/com/glitch/securenotes/domain/rooms/noteslist/NotesListSocketEvent.kt
@@ -1,20 +1,74 @@
 package com.glitch.securenotes.domain.rooms.noteslist
 
+import com.glitch.securenotes.data.model.entity.NoteModel
 import kotlinx.serialization.Serializable
 
+//@Serializable
+//data class NotesListSocketEvent(
+//    val initiatedUser: String,
+//    val eventType: Short,
+//    val affectedNoteIds: List<String>
+//) {
+
+//}
+
 @Serializable
-data class NotesListSocketEvent(
-    val initiatedUser: String,
-    val eventType: Short,
-    val affectedNoteIds: List<String>
-) {
+sealed class NotesListSocketEvent {
+    abstract val initiatedUserId: String
+    abstract val eventCode: Short
+    abstract val affectedNoteIds: List<String>?
+    abstract val newRoleCode: Short?
+    abstract val affectedNoteModel: NoteModel?
+
+    @Serializable
+    data class NewNote(
+        override val initiatedUserId: String,
+        override val affectedNoteModel: NoteModel
+    ): NotesListSocketEvent() {
+        override val eventCode = EVENT_ADDED_NEW_NOTE
+        override val affectedNoteIds = null
+        override val newRoleCode = null
+    }
+
+    @Serializable
+    data class UpdatedNote(
+        override val initiatedUserId: String,
+        override val affectedNoteModel: NoteModel?
+    ): NotesListSocketEvent() {
+        override val eventCode = EVENT_UPDATED_NOTE
+        override val affectedNoteIds = null
+        override val newRoleCode = null
+    }
+
+    @Serializable
+    data class DeletedNote(
+        override val initiatedUserId: String,
+        override val affectedNoteIds: List<String>
+    ): NotesListSocketEvent() {
+        override val eventCode = EVENT_DELETED_NOTE
+        override val newRoleCode = null
+        override val affectedNoteModel = null
+    }
+
+    @Serializable
+    data class UpdatedRole(
+        override val initiatedUserId: String,
+        val affectedNoteId: String,
+        override val newRoleCode: Short
+    ): NotesListSocketEvent() {
+        override val eventCode = EVENT_UPDATED_ROLE
+        override val affectedNoteIds = listOf(affectedNoteId)
+        override val affectedNoteModel = null
+    }
+
     companion object {
-        const val ADDED_NEW_NOTE: Short = 1
-        const val UPDATED_NOTE: Short = 2
-        const val UPDATED_NOTE_PREVIEW: Short = 3
-        const val DELETED_NOTE: Short = 4
-        const val UPDATED_ROLE_TO_EDITOR: Short = 5
-        const val UPDATED_ROLE_TO_READER: Short = 6
-        const val UPDATED_ROLE_TO_OWNER: Short = 7
+        private const val EVENT_ADDED_NEW_NOTE: Short = 1
+        private const val EVENT_UPDATED_NOTE: Short = 2
+        private const val EVENT_DELETED_NOTE: Short = 3
+        private const val EVENT_UPDATED_ROLE: Short = 4
+
+        const val ROLE_OWNER: Short = 1
+        const val ROLE_EDITOR: Short = 2
+        const val ROLE_READER: Short = 3
     }
 }
